29a2b3ce526647a84dcdeec0b4938c990ebbf923
hampusjohansson1 <hampus.johansson13@gmail.com>
2017-04-07 10:43:13 +0200

    Created choose number of destinations screen

--- app/src/main/java/com/example/wapp/MainActivity.java
+++ app/src/main/java/com/example/wapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.wapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void StartGame(View view) {
+        Intent intent = new Intent(this, SelectDest.class);
+        startActivity(intent);
+    }
+
 }
